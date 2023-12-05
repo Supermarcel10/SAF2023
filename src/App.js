c2a2d1c69e9f9609b0568862764efc2e1535c2dc
marcel <marcelbarlik@gmail.com>
2023-12-05 07:36:12 +0000

    Updated general look

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Choice from "./Choice";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <p id={"sentence"}>This is a story about a little <span className={"important"}>cat</span>.</p>
+        </div>
+        <div>
+          {/*<Choice image_path={""} image_alt={""} />*/}
+        </div>
+        <div>
+          <button className={"nav-button"} id={"previous-button"}>&#8592;</button>
+          <button className={"nav-button"} id={"next-button"}>&#8594;</button>
+        </div>
       </header>
     </div>
   );
