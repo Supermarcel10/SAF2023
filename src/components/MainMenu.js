4acd7a52d179dd90dc7f837bc66a8a17cc62d57c
ellentaylor827 <101396715+ellentaylor827@users.noreply.github.com>
2023-12-05 19:12:37 +0000

    Add files via upload

--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -1,32 +1,48 @@
 import '../styles/MainMenu.css'
 import MenuChoice from '../components/MenuChoice.tsx'
+import React from 'react'
 
 import catImage from '../img/cat.png';
 import dogImage from '../img/dog.png';
 import horseImage from '../img/horse.png';
 import beeImage from '../img/bee.png';
 import frogImage from '../img/frog.png';
+import logo from '../img/logo.png';
+
+//import App from '../App.js';
+const LazyApp = React.lazy(() => import('../App.js'));
+
+function ImageClickLoader() {
+  const handleMenuChoiceClick = () => {
+  // Dynamically load App component
+  LazyApp.preload();
+
+  // Your logic for handling the click event goes here
+  console.log('MenuChoice clicked!');}}
 
 function MainMenu() {
-  return (
-    <div className = "MainMenu">
-      <header className="App-header">
-        <div className={"sentenceContainer"}>
-          <p id={"title"}>Felines & Fables</p>
-        </div>
-        <div>
-          <p id={"sentence"}>Select a story:</p>
-        </div>
-        <div className={"selector"}>
-          <MenuChoice english={"Coco The Curious Cat"} translated={"Coco Maraqlı Pişik"} image_path={catImage} image_alt={"image of a cat"} />
-          <MenuChoice english={"Daisy's Digging Day"} translated={"Daisy'nin Qazma Günü"} image_path={dogImage} image_alt={"image of a dog"} />
-          <MenuChoice english={"Cinnamon's Wild Ride"} translated={"Darçın Vəhşi Sürüş"} image_path={horseImage} image_alt={"image of a horse"} />
-          <MenuChoice english={"Happy Honey Harvest"} translated={"Xoşbəxt Bal Məhsulu"} image_path={beeImage} image_alt={"image of a bee"} />
-          <MenuChoice english={"Frog finds a friend"} translated={"Qurbağa dost tapır"} image_path={frogImage} image_alt={"image of a frog"} />
-        </div>
-      </header>
-    </div>
-  );
-}
+    return (
+      <div className = "MainMenu">
+          <header className="App-header">
+          <div>
+            <img className="logoImage" src={logo} alt = "Logo image"></img>
+          </div>
+            <div className={"sentenceContainer"}>
+              <p id={"title"}>Felines & Fables</p>
+            </div>
+            <div>
+              <p id={"sentence"}>Select a story:</p>
+            </div>
+            <div> <p id = {"Sentence"}> Coins: 0</p></div>
+            <div className={"selector"}>
+              <MenuChoice english={"Coco The Curious Cat"} translated={"Coco Maraqlı Pişik"} image_path={catImage} image_alt={"image of a cat"} onClick={ImageClickLoader}/>
+              <MenuChoice english={"Daisy's Digging Day"} translated={"Daisy'nin Qazma Günü"} image_path={dogImage} image_alt={"image of a dog"} status = "locked" />
+              <MenuChoice english={"Cinnamon's Wild Ride"} translated={"Darçın Vəhşi Sürüş"} image_path={horseImage} image_alt={"image of a horse"} status = "locked"/>
+              <MenuChoice english={"Frog finds a friend"} translated={"Qurbağa dost tapır"} image_path={frogImage} image_alt={"image of a frog"} status = "locked"/>
+            </div>
+          </header>
+      </div>
+    );
+  }
 
   export default MainMenu;
--- src/components/MenuChoice.tsx
+++ src/components/MenuChoice.tsx
@@ -6,14 +6,20 @@ interface MenuChoiceProps {
 	translated: string;
 	image_path: string;
 	image_alt: string;
+	status: string;
+	onClick: () => void;
 }
 
-function MenuChoice(props: MenuChoiceProps) {
+function MenuChoice(props: MenuChoiceProps ) {
+	const statusColor = props.status === 'locked' ? 'var(--red)' : 'var(--small-font-color)' ;
 	return (
-		<div className="selector">
-			<img className="image" src={props.image_path} alt={props.image_alt} />
-			<p className="english">{props.english}</p>
-			<p className={"translation"}>{props.translated}</p>
+
+		<div className="selector"  onClick={props.onClick}>
+			<div className="textContainer" style={{ backgroundColor: statusColor}}>
+			<img className="image" src={props.image_path} alt={props.image_alt}/>
+				<p className="english">{props.english}</p>
+				<p className="translation">{props.translated}</p>
+      		</div>
 		</div>
 	);
 }
