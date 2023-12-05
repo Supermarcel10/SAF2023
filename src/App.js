f318956cb621d54b9e0f238c60de622c9459ab69
marcel <marcelbarlik@gmail.com>
2023-12-05 09:57:05 +0000

    Part-commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
-import './App.css';
-import Choice from "./components/Choice";
+import './styles/App.css';
+import Choice from './components/Choice.tsx'
+
+import catImage from './img/cat.png';
+import dogImage from './img/dog.png';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div>
-          <p id={"sentence"}>This is a <span className={"blank"}>story</span> about a little <span className={"important"}>cat</span>.</p>
+        <div className={"sentenceContainer"}>
+          <p id={"sentence"}>This is a <span className={"blank"}>story</span> about a little cat. This is a slightly longer story. That spans multiple lines.</p>
         </div>
-        <div>
-          {/*<Choice english={"Cat"} translated={"pişik"} image_path={"src/img/ball.png"} image_alt={"cat"} />*/}
+        <div className={"selector"}>
+          <Choice english={"cat"} translated={"pişik"} image_path={catImage} image_alt={"image of a cat"} />
+          <Choice english={"dog"} translated={"it"} image_path={dogImage} image_alt={"image of a dog"} />
+          <Choice english={"dog"} translated={"it"} image_path={dogImage} image_alt={"image of a dog"} />
+          <Choice english={"dog"} translated={"it"} image_path={dogImage} image_alt={"image of a dog"} />
         </div>
-        <div>
+        <div className={"navigation"}>
           <button className={"nav-button"} id={"previous-button"}>&#8592;</button>
           <button className={"nav-button"} id={"next-button"}>&#8594;</button>
         </div>
